955b9fd2c5330a085b2919f2cfe2d012c1286c16
Yoshi-158 <nnyoshitha@gmail.com>
2026-05-06 11:09:33 +0530

    Update queries.sql

--- week1/day1/queries.sql
+++ week1/day1/queries.sql
@@ -12,3 +12,18 @@ SELECT name FROM Department;
 
 -- Q5: Select employees who work in the IT department
 SELECT * FROM Employee WHERE department_id = 1;
+
+-- Q6: Select employees whose names start with 'J'
+SELECT * FROM Employee WHERE name LIKE 'J%';
+
+-- Q7: Select employees whose names end with 'e'
+SELECT * FROM Employee WHERE name LIKE '%e';
+
+-- Q8: Select employees whose names contain 'a'
+SELECT * FROM Employee WHERE name LIKE '%a%';
+
+-- Q9: Select employees whose names are exactly 9 characters long
+SELECT * FROM Employee WHERE name LIKE '_________';
+
+-- Q10: Select employees whose names have 'o' as the second character
+SELECT * FROM Employee WHERE name LIKE '_o%';
